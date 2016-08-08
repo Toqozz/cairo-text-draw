2e30bd508799556662c0cc9fdddf95dbe9d86be8
Michael Palmos <toqoz@hotmail.com>
2016-08-08 15:06:53 +1000

    commit datatypes, oops

---
+++ datatypes.h
@@ -0,0 +1,22 @@
+struct Variables {
+    char *font;
+    int   margin;
+    int   number;
+    int   upper;
+    int   gap;
+    int   rounding;
+    int   timeout;
+    int   xpos;
+    int   ypos;
+    int   width;
+    int   height;
+};
+
+struct MessageInfo {
+    char *string;
+    int   textx;
+    int   texty;
+    int   x;
+    int   y;
+    long double fuse;
+};
